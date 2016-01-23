8948ac49fbabacecfabd75b0c0e9e32e0b54bb91
Matteo Collina <hello@matteocollina.com>
2016-01-23 11:46:12 +0100

    Trying to fix v0.10 on Travis.

--- test.js
+++ test.js
@@ -32,50 +32,51 @@ abs({
 test('multiple persistences', function (t) {
   t.plan(7)
   db.flushall()
+  setImmediate(function () {
+    var emitter = mqemitterRedis()
+    var emitter2 = mqemitterRedis()
+    var instance = persistence()
+    var instance2 = persistence()
+    instance.broker = {
+      id: '1',
+      publish: emitter.emit.bind(emitter),
+      subscribe: emitter.on.bind(emitter)
+    }
+    instance2.broker = {
+      id: '2',
+      publish: emitter2.emit.bind(emitter2),
+      subscribe: emitter2.on.bind(emitter2)
+    }
+    var client = { id: 'abcde' }
+    var subs = [{
+      topic: 'hello',
+      qos: 1
+    }, {
+      topic: 'hello/#',
+      qos: 1
+    }, {
+      topic: 'matteo',
+      qos: 1
+    }]
 
-  var emitter = mqemitterRedis()
-  var emitter2 = mqemitterRedis()
-  var instance = persistence()
-  var instance2 = persistence()
-  instance.broker = {
-    id: '1',
-    publish: emitter.emit.bind(emitter),
-    subscribe: emitter.on.bind(emitter)
-  }
-  instance2.broker = {
-    id: '2',
-    publish: emitter2.emit.bind(emitter2),
-    subscribe: emitter2.on.bind(emitter2)
-  }
-  var client = { id: 'abcde' }
-  var subs = [{
-    topic: 'hello',
-    qos: 1
-  }, {
-    topic: 'hello/#',
-    qos: 1
-  }, {
-    topic: 'matteo',
-    qos: 1
-  }]
-
-  instance.addSubscriptions(client, subs, function (err) {
-    t.notOk(err, 'no error')
-    instance2.subscriptionsByTopic('hello', function (err, resubs) {
+    instance.addSubscriptions(client, subs, function (err) {
       t.notOk(err, 'no error')
-      t.deepEqual(resubs, [{
-        clientId: client.id,
-        topic: 'hello/#',
-        qos: 1
-      }, {
-        clientId: client.id,
-        topic: 'hello',
-        qos: 1
-      }])
-      instance.destroy(t.pass.bind(t, 'first dies'))
-      instance2.destroy(t.pass.bind(t, 'second dies'))
-      emitter.close(t.pass.bind(t, 'first emitter dies'))
-      emitter2.close(t.pass.bind(t, 'second emitter dies'))
+      instance2.subscriptionsByTopic('hello', function (err, resubs) {
+        t.notOk(err, 'no error')
+        t.deepEqual(resubs, [{
+          clientId: client.id,
+          topic: 'hello/#',
+          qos: 1
+        }, {
+          clientId: client.id,
+          topic: 'hello',
+          qos: 1
+        }])
+        instance.destroy(t.pass.bind(t, 'first dies'))
+        instance2.destroy(t.pass.bind(t, 'second dies'))
+        emitter.close(t.pass.bind(t, 'first emitter dies'))
+        emitter2.close(t.pass.bind(t, 'second emitter dies'))
+      })
     })
   })
 })
