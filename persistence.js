d063f5514bb0e3fd4c755395a548f6231e2461da
Gavin Dmello <dmellogavin5000@gmail.com>
2017-02-19 15:37:31 +0530

    Wrapped prekey in list ref

--- persistence.js
+++ persistence.js
@@ -360,19 +360,23 @@ function processKeysForClient (all, that) {
 }
 
 RedisPersistence.prototype.outgoingEnqueue = function (sub, packet, cb) {
-  var key = 'outgoing:' + sub.clientId + ':' + packet.brokerId + ':' + packet.brokerCounter
-  this._getPipeline().set(key, msgpack.encode(new Packet(packet)), cb)
+  var listKey = 'outgoing:' + sub.clientId
+  var key = listKey + ':' + packet.brokerId + ':' + packet.brokerCounter
+
+  var multi = this._db.multi()
+  multi.set(key, msgpack.encode(new Packet(packet)))
+  multi.rpush(listKey, key)
+
+  multi.exec(cb)
 }
 
 function updateWithClientData (that, client, packet, cb) {
   var prekey = 'outgoing:' + client.id + ':' + packet.brokerId + ':' + packet.brokerCounter
-  var listKey = 'outgoing-id:' + client.id
   var postkey = 'outgoing-id:' + client.id + ':' + packet.messageId
 
   var multi = that._db.multi()
   multi.set(postkey, msgpack.encode(packet))
   multi.set(prekey, msgpack.encode(packet))
-  multi.rpush(listKey, postkey)
 
   multi.exec(function execMulti (err, results) {
     if (err) { return cb(err, client, packet) }
@@ -418,8 +422,9 @@ RedisPersistence.prototype.outgoingUpdate = function (client, packet, cb) {
 
 RedisPersistence.prototype.outgoingClearMessageId = function (client, packet, cb) {
   var that = this
-  var listKey = 'outgoing-id:' + client.id
+  var listKey = 'outgoing:' + client.id
   var key = 'outgoing-id:' + client.id + ':' + packet.messageId
+
   this._getPipeline().getBuffer(key, function clearMessageId (err, buf) {
     if (err) {
       return cb(err)
@@ -430,12 +435,12 @@ RedisPersistence.prototype.outgoingClearMessageId = function (client, packet, cb
     }
 
     var packet = msgpack.decode(buf)
-    var prekey = 'outgoing:' + client.id + ':' + packet.brokerId + ':' + packet.brokerCounter
+    var prekey = listKey + ':' + packet.brokerId + ':' + packet.brokerCounter
 
     var multi = that._db.multi()
     multi.del(key)
     multi.del(prekey)
-    multi.lrem(listKey, 1, key)
+    multi.lrem(listKey, 1, prekey)
 
     multi.exec(function execOps (err) {
       cb(err, packet)
@@ -454,7 +459,7 @@ RedisPersistence.prototype.outgoingStream = function (client) {
   return new RangeStream({
     objectMode: true,
     redis: this._db,
-    key: 'outgoing-id:' + client.id
+    key: 'outgoing:' + client.id
   }).pipe(through.obj(split))
     .pipe(throughv.obj(this._decodeAndAugment))
 }
